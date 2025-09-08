154e2a7372c18e2e8483c76ae5ca3dd0298a997a
Horros <minh.chenh.learning@gmail.com>
2025-09-08 20:29:28 +0700

    Update currency_converter.py

--- Currency_converter/currency_converter.py
+++ Currency_converter/currency_converter.py
@@ -1,11 +1,33 @@
 import requests
 
+def default_cache(currency):
+    global cache
+    cache = {}
+    url = f"https://www.floatrates.com/daily/{currency}.json"   
+    try:
+        global r    
+        r = requests.get(url).json()
+        if currency != "usd":
+            cache["usd"] = r["usd"]["rate"]
+        if currency != "EUR":
+            cache["eur"] = r["eur"]["rate"]
+    except requests.exceptions.ConnectionError:
+        print("Connection Failed")
+def converting(currency, convert_to, amount):
+    print("Cheking in the cache...")
+    if convert_to in list(cache.keys()):
+        print("Oh! It is in the cache!")
+    else:
+        print("Sorry, but it is not in the cache!")
+        cache[convert_to] = r[convert_to]["rate"]
+    converted = round(amount*cache[convert_to], 2)
+    print(f"You received {converted} {convert_to.upper()}.")
+
 currency = input().lower()
-url = f"https://www.floatrates.com/daily/{currency}.json"   
-try:    
-    r = requests.get(url).json()
-    usd_rate = r["usd"]
-    eur_rate = r["eur"]
-    print(usd_rate, eur_rate, sep="\n")
-except requests.exceptions.ConnectionError:
-    print("Connection Failed")
+while True:
+    convert_to = input().lower()
+    if convert_to == "":
+        break
+    amount = float(input())
+    default_cache(currency)
+    converting(currency, convert_to, amount)
