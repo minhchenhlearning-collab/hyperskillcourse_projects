1c555da18d1d8476c1dfdcac0688ee1018cd8f30
minhchenhlearning-collab <minh.chenh.learning@gmail.com>
2025-09-08 13:49:40 +0700

    Delete Currency_converter/currency_converter.py

--- Currency_converter/currency_converter.py
+++
@@ -1,5 +0,0 @@
-# write your code here!
-conicoins = float(input("Please, enter the number of conicoins you have:"))
-currency_value = {"RUB":2.98, "ARS":0.82, "HNL":0.17, "AUD":1.9622, "MAD":0.208}
-for currency, value in currency_value.items():
-    print(f"I will get {value} {currency} from the sale of {conicoins} conicoins.")
